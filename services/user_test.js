189c63f4bd7bfed5aa17d28f84d698fab9c74e42
test <mani95lisa@gmail.com>
2014-03-04 17:22:55 +0800

    update return results

--- services/user_test.js
+++ services/user_test.js
@@ -4,7 +4,7 @@ module.exports.update = function(req){
     var payload = req.query;
     UserTest.update({user:payload.u}, {config:payload.c}, {upsert:true}, function(err, result){
         if(err){
-            req.reply({status:false, result:err.code});
+            req.reply({status:false, results:err.code});
         }else{
             req.reply({status:true});
         }
@@ -14,11 +14,11 @@ module.exports.update = function(req){
 module.exports.getALL = function(req){
     UserTest.find({}, function (err, result) {
         if(err){
-            req.reply({status:false, result:err.code});
+            req.reply({status:false, results:err.code});
         }else if(result){
-            req.reply({status:true, result: result});
+            req.reply({status:true, results: result});
         }else{
-            req.reply({status:false, result:'没有用户存在'});
+            req.reply({status:false, results:'没有用户存在'});
         }
     });
 }
@@ -28,17 +28,17 @@ module.exports.getConfig = function(req) {
     var q = req.query;
     if(!q.u)
     {
-        req.reply({status:false, result:'u参数不能为空'});
+        req.reply({status:false, results:'u参数不能为空'});
         return;
     }
     UserTest.findOne({user: q.u}, function(err, result){
     	if(err){
-    		req.reply({status:false, result:err.code});
+    		req.reply({status:false, results:err.code});
     	}else if(result){
-    		req.reply({status:true, result: result.config});
+    		req.reply({status:true, results: result.config});
 			console.log(result.config);
     	}else{
-            req.reply({status:false, result:'该用户不存在'});
+            req.reply({status:false, results:'该用户不存在'});
         }
     });
 };
